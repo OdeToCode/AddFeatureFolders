d4359d691ec51cd148b44ebb5e57948518103f60
Ken Bonny <bonny.ken+github@gmail.com>
2018-07-18 21:17:31 +0200

    Update how views in areas are found

--- src/OdeToCode.AddFeatureFolders/AreaFeatureFolderOptions.cs
+++ src/OdeToCode.AddFeatureFolders/AreaFeatureFolderOptions.cs
@@ -5,13 +5,21 @@
         /// <summary>
         /// Options to control the area feature folders
         /// </summary>
-        public AreaFeatureFolderOptions()
+        public AreaFeatureFolderOptions() : this(new FeatureFolderOptions())
         {
-            AreaFolderName = "Areas";
-            DefaultAreaViewLocation = @"\Areas\{2}\{1}\{0}.cshtml";
-            FeatureFolderName = "Features";
         }
         
+        /// <summary>
+        /// Options to control the area feature folders
+        /// </summary>
+        /// <param name="options">Can read options from <see cref="FeatureFolderOptions"/> if non default options are chosen</param>
+        public AreaFeatureFolderOptions(FeatureFolderOptions options)
+        {
+            AreaFolderName = "Areas";
+            DefaultAreaViewLocation = $@"Areas\{{2}}\{options.FeatureNamePlaceholder}\{{0}}.cshtml";
+            FeatureFolderName = options.FeatureFolderName;
+        }
+
         /// <summary>
         /// The name of the root area folder on disk (default: 'Areas')
         /// </summary>
@@ -19,8 +27,10 @@
         
         /// <summary>
         /// The default view location for areas. Helps intellisense find razor views. Example:
-        ///     "\Areas\{2}\{1}\{0}.cshtml". 
-        /// Razor replaces the area name into {2} placeholder, the controller name into {1} placeholder & view name into the {0} placeholder. 
+        ///     "\Areas\{2}\{Feature}\{0}.cshtml". 
+        /// Razor replaces the area name into {2} placeholder, the controller name into {1} placeholder & view name into the {0} placeholder.
+        /// The {Feature} placeholder must be the same name as the <see cref="FeatureFolderOptions"/> FeatureNamePlaceholder to replace
+        /// the placeholder with the path to the feature folder.
         /// </summary>
         public string DefaultAreaViewLocation { get; set; }
 
--- src/OdeToCode.AddFeatureFolders/ServiceCollectionExtensions.cs
+++ src/OdeToCode.AddFeatureFolders/ServiceCollectionExtensions.cs
@@ -20,7 +20,7 @@ namespace Microsoft.Extensions.DependencyInjection
 
             var expander = new FeatureViewLocationExpander(options);
 
-            services.AddMvcOptions(o => { o.Conventions.Add(new FeatureControllerModelConvention(options)); })
+            services.AddMvcOptions(o => o.Conventions.Add(new FeatureControllerModelConvention(options)))
                 .AddRazorOptions(o =>
                 {
                     o.ViewLocationFormats.Clear();
@@ -42,9 +42,10 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddRazorOptions(o =>
             {
                 o.AreaViewLocationFormats.Clear();
-                o.AreaViewLocationFormats.Add(options.AreaFolderName + @"\{2}\{0}.cshtml");
-                o.AreaViewLocationFormats.Add(options.AreaFolderName + @"\Shared\{0}.cshtml");
                 o.AreaViewLocationFormats.Add(options.DefaultAreaViewLocation);
+                o.AreaViewLocationFormats.Add(options.AreaFolderName + @"\{2}\{1}\{0}.cshtml");
+                o.AreaViewLocationFormats.Add(options.AreaFolderName + @"\{2}\Shared\{0}.cshtml");
+                o.AreaViewLocationFormats.Add(options.AreaFolderName + @"\Shared\{0}.cshtml");
                 o.AreaViewLocationFormats.Add(options.FeatureFolderName + @"\Shared\{0}.cshtml");
             });
 
